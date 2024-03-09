903e27adf7c885c7748bdedd5f049581a7bfd918
Почемучка <159887975+Maria-Kipurova@users.noreply.github.com>
2024-03-10 00:21:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,44 +3,56 @@
 #include "alg.h"
 
 double pown(double value, uint16_t n) {
-    double result = 1.0;
-    for (uint16_t i = 0; i < n; i++) {
-        result *= value;
+    double k = 1;
+    while (n > 0) {
+        k = value * k;
+        n--;
     }
-    return result;
+    return k;
 }
 
 uint64_t fact(uint16_t n) {
-    if (n == 0) {
-        return 1;
+    uint64_t k = 1;
+    for (int i = 1; i <= n; ++i) {
+        k *= i;
     }
-    return n * fact(n - 1);
+    return k;
 }
 
 double calcItem(double x, uint16_t n) {
-    return pown(x, n) / fact(n);
+    double k = pown(x, n) / fact(n);
+    return k;
 }
 
 double expn(double x, uint16_t count) {
-    double result = 0.0;
-    for (uint16_t n = 0; n < count; n++) {
-        result += calcItem(x, n);
+    uint16_t x1 = abs(count);
+    double xexe = 1;
+    if (count == 0)
+        return 0;
+    for (uint16_t i = 1; i <= x1; ++i) {
+        xexe += calcItem(x, i);
     }
-    return result;
+    return xexe;
 }
 
 double sinn(double x, uint16_t count) {
-    double result = 0.0;
-    for (uint16_t n = 0; n < count; n++) {
-        result += (n % 2 == 0 ? 1 : -1) * calcItem(x, 2 * n + 1);
+    uint16_t x1 = abs(count);
+    double k = 0;
+    if (count == 0)
+        return 0;
+    for (uint16_t i = 1; i <= x1; ++i) {
+        k += pown(-1, i - 1) * pown(x, 2 * i - 1) / fact(2 * i - 1);
     }
-    return result;
+    return k;
 }
 
 double cosn(double x, uint16_t count) {
-    double result = 0.0;
-    for (uint16_t n = 0; n < count; n++) {
-        result += (n % 2 == 0 ? 1 : -1) * calcItem(x, 2 * n);
+    uint16_t x1 = abs(count);
+    double k = 0;
+    if (count == 0)
+        return 0;
+    for (uint16_t i = 1; i <= x1; ++i) {
+        k += pown(-1, i - 1) * pown(x, 2 * i - 2) / fact(2 * i - 2);
     }
-    return result;
+    return k;
 }
